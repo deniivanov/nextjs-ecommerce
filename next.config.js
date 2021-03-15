954071386d52b1be094fb75732f30621c6bbacfb
deniivanov <theodlo@gmail.com>
2021-03-15 20:05:01 +0200

    running version of the e-store

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    env: {
-        "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "YOUR_MONGODB_URL",
-        "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
-        "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
-        "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
-        "CLOUD_UPDATE_PRESET": "YOUR_CLOUD_UPDATE_PRESET",
-        "CLOUD_NAME": "YOUR_CLOUD_NAME",
-        "CLOUD_API": "YOUR_CLOUD_API"
-    }
-}
+  env: {
+    BASE_URL: "http://localhost:3000",
+    MONGODB_URL:
+      "mongodb+srv://passport:ojVfgx7zbVZpa6T5@cluster0-hwpmi.mongodb.net/autostore",
+    ACCESS_TOKEN_SECRET: "YOUR_ACCESS_TOKEN_SECRET",
+    REFRESH_TOKEN_SECRET: "YOUR_REFRESH_TOKEN_SECRET",
+    PAYPAL_CLIENT_ID: "YOUR_PAYPAL_CLIENT_ID",
+    CLOUD_UPDATE_PRESET: "YOUR_CLOUD_UPDATE_PRESET",
+    CLOUD_NAME: "dvecmkagx",
+    CLOUD_API: "637247725563715",
+  },
+};
