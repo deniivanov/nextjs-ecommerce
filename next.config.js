cfd412364ac2984abf0f468b66ad95f99c71661e
deniivanov <theodlo@gmail.com>
2021-05-19 13:53:19 +0300

    security fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   env: {
-    BASE_URL: "http://localhost:3000",
-    MONGODB_URL:
-      "mongodb+srv://passport:ojVfgx7zbVZpa6T5@cluster0-hwpmi.mongodb.net/autostore",
+    BASE_URL: "http://vitalize.bg",
+    MONGODB_URL: process.env.MONGODB,
     ACCESS_TOKEN_SECRET: "SGe36lEOIxWAHE5VLZKRJWlKb40",
     REFRESH_TOKEN_SECRET: "YOUR_REFRESH_TOKEN_SECRET",
     PAYPAL_CLIENT_ID: "YOUR_PAYPAL_CLIENT_ID",
